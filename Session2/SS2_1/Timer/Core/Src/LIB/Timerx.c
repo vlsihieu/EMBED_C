48c68dd232b2d59865e16e8366b22216cbaef737
anhtu2212 <tulha1689@ut.edu.vn>
2026-06-11 22:20:02 +0700

    Update Timerx.c

--- Session2/SS2_1/Timer/Core/Src/LIB/Timerx.c
+++ Session2/SS2_1/Timer/Core/Src/LIB/Timerx.c
@@ -26,8 +26,8 @@ void Timer2_4_IT_Config(TIM_TypeDef *TIMx)
 //  PSC  = 7200-1
 //  Config timer 1s ?
 
-	TIMx->ARR = 10000 ;  // 1s
-	TIMx->PSC = 8000- 1;
+	TIMx->ARR = 10000;  
+	TIMx->PSC = 7200- 1;
 
 	//  Direction
 	TIMx->CR1 &= ~(1 << 4); // Mode counter up
@@ -47,6 +47,15 @@ void delay_ms(TIM_TypeDef *TIMx, uint16_t ms)
 	// Reset CNT về 0
 	TIMx ->CNT = 0;
 	// CNT chạy cho đến khi bằng số ms cụ thể
+	/*  
+	Để set time 1s
+	Chọn PSC= 7200 để chia nhỏ clock :
+	   F = 72M/7200 = 10000 cout/ 1s 
+	Chọn ARR = 10000:
+      1 cout = 0.1ms
+	  => 0.1ms x 10000 = 1s
+
+	*/ 
 	while (TIMx ->CNT < ms);
 
 }
