3a0850e40108815e484b67a771069d9a8bb372b3
Duong Dinh Tu <tudd0645@ut.edu.vn>
2026-07-24 21:22:33 +0700

    [Timer][DuongTu]Input Capture

---
+++ Session4/SS4_3/Core/Src/Lib/Capture/capture.h
@@ -0,0 +1,14 @@
+
+#ifndef SRC_LIB_CAPTURE_CAPTURE_H_
+#define SRC_LIB_CAPTURE_CAPTURE_H_
+
+
+void capture_init();
+uint32_t capture_read(void);
+uint32_t capture_pulse_width1();
+uint32_t capture_pulse_width2();
+uint32_t capture_pulse_width3();
+uint32_t capture_pulse_width4();
+
+
+#endif /* SRC_LIB_CAPTURE_CAPTURE_H_ */
---
+++ Session4/SS4_3/Core/Src/Lib/PWM/pwm.h
@@ -0,0 +1,9 @@
+
+#ifndef SRC_LIB_PWM_PWM_H_
+#define SRC_LIB_PWM_PWM_H_
+
+// Initializes PWM output signal: Frequency, Duty Cycle
+void PWM_init();
+
+
+#endif /* SRC_LIB_PWM_PWM_H_ */
