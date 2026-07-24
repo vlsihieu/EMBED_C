e6b2a1c0f0e79fe2d79223834f1c3206cb5a7065
Duong Dinh Tu <tudd0645@ut.edu.vn>
2026-07-24 21:13:33 +0700

    [Timer][DuongTu]Input Capture

---
+++ Session4/timer_exercise_24_7/Core/Src/Lib/Delay/timer.c
@@ -0,0 +1,46 @@
+
+#include"TIMER.H"
+void Timer2_4_IT_Config(TIM_TypeDef *TIMx)
+{
+	// Clock enable
+	if(TIMx == TIM2)
+	{
+		RCC->APB1ENR |= 1 << 0;
+	}
+	else if(TIMx == TIM4)
+	{
+		RCC->APB1ENR |= 1 << 3;
+	}
+
+//	config 1s
+//	TIMx->ARR = 10000;  // 1s
+//	TIMx->PSC = 7200 - 1;
+//	APB1 = 8MHz
+//  ARR  = 10000-1
+//  PSC  = 8000-1
+//  Config timer 1s ?
+
+	TIMx->ARR = 10000-1;  // 1s
+	TIMx->PSC = 8000- 1;
+
+	//  Direction
+	TIMx->CR1 &= ~(1 << 4); // Mode counter up
+	// Update generation
+	TIMx->EGR |= (1 << 0);
+
+	// Counter enable
+	TIMx->CR1 |= (1 << 0);
+
+}
+
+//  Delay
+void delay_ms(TIM_TypeDef *TIMx, uint16_t ms)
+{
+	// Reset CNT về 0
+	TIMx ->CNT = 0;
+	// F=8Mhz PSC= 8000 --> t'=1ms
+	while (TIMx->CNT < (uint32_t)ms);
+
+}
+
+
